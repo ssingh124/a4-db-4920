c748807c2f696b08da3fd4e33e9df9e3b8477cf3
EC2 Default User <ec2-user@ip-172-31-93-203.ec2.internal>
2019-11-21 21:19:18 +0000

    Initial Commit

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+require('dotenv').config({path: './../.env'});
+
+const mysql = require('mysql');
+
+const connectionPool = mysql.createPool({
+  connectionLimit:10,	
+  host     : process.env.DB_HOST,		
+  user     : process.env.DB_USER,	
+  password : process.env.DB_PASS,   
+  database : process.env.DB_NAME,
+  multipleStatements: true	
+});
+
+exports.cp = connectionPool;
